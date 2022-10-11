9fbf82611acf78778090aa4100f8a2d8df6a50af
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-10-11 15:19:56 -0700

    Update nibble_solution.cpp

--- nibble_solution.cpp
+++ nibble_solution.cpp
@@ -1,4 +1,4 @@
- 
+  
 // This file was written by cfiddle.  It's likely that it'll get overwritten in the future, so any edits you make are likely to be lost.
 
 #include <vector>
@@ -9,14 +9,11 @@
 extern "C" uint64_t nibble_search(uint8_t query, const std::vector<uint16_t> & targets) {
     uint64_t count = 0;
 
-    for(auto & i: targets) {
-        for(int b = 0; b < 16 - 4; b++) {
-            uint8_t extracted = ((i >> b) & (0xf));
-            if (extracted == query) {
-                count+=1;
-            }
+    for(uint16_t i: targets) {
+        for(int b = 0; b < 12; b++) {
+            count += (uint64_t) ((i) & (0xf)) == query;
+            i >>= 1;
         }
     }
     return count;
 }
-
